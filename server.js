6b9b8bcb867b18f4ecb910c024dc43bf76a24d9f
Artur <ghostmatt36@gmail.com>
2022-07-02 23:18:02 +0300

    part 5 + yarn

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require ('http');
+let counter = 0
 
 const server = http.createServer((request, response) => {
+    counter++
    switch (request.url) {
        case '/':
-           response.write ('HomePage')
+           response.write ('HomePage' + counter)
            break;
        case '/student':
            response.write ('Hello students!')
